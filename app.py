f4b4c7176ebc836adcf9fbeb5a30381e8aa61beb
GowthamBharathn <barathinfo28@gmail.com.com>
2025-07-27 21:19:46 +0530

    adding chat route

--- app.py
+++ app.py
@@ -1,9 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
@@ -11,6 +6,12 @@ app = Flask(__name__)
 def home():
     return "Hello from Infinity2.0! 🚀"
 
+@app.route("/chat", methods=["POST"])
+def chat():
+    data = request.json
+    user_message = data.get("message", "")
 
+    # TODO: Replace this with your real AI model later
+    bot_reply = f"Infinity2.0 received: '{user_message}' 🤖"
 
-
+    return jsonify({"response": bot_reply})
